73f1c726412ac638ae604accb6bf6546f4d2cab4
Temilola Aderibigbe <temz4rill@gmail.com>
2025-01-17 13:39:27 -0500

    implemented new support for mock contact generation

--- dummy-contacts.js
+++ dummy-contacts.js
@@ -1,58 +1,58 @@
 const fs = require(`fs`)
 const path = require(`path`)
-const faker = require(`faker`)
-const base64Img = require(`base64-img`)
+let fakerv0 = require(`faker`)
+let faker = require(`@faker-js/faker`)
+const imageToBase64 = require('image-to-base64');
 
-const numOfContactsToGenerate = 2
+const numOfContactsToGenerate = 500
 
-const contactsFile = path.resolve(`./dummy-contacts.vcf`)
+const contactsFile = path.resolve(`./all.vcf`)
 
 fs.writeFileSync(contactsFile, ``)
 
 for (let i = 1; i <= numOfContactsToGenerate; i++) {
-  const firstName = faker.name.firstName()
-  const lastName = faker.name.lastName()
+  const firstName = faker.faker.person.firstName()
+  const lastName = faker.faker.person.lastName()
 
   const address1 = {
-    street: faker.address.streetAddress(),
-    city: faker.address.city(),
-    stateAbbr: faker.address.stateAbbr(),
-    zipCode: faker.address.zipCode(),
-    country: faker.address.country(),
+    street: faker.faker.location.streetAddress(),
+    city: faker.faker.location.city(),
+    stateAbbr: faker.faker.location.state({ abbreviated: true }),
+    zipCode: faker.faker.location.zipCode(),
+    country: faker.faker.location.country()
   }
 
   const address2 = {
-    street: faker.address.streetAddress(),
-    city: faker.address.city(),
-    stateAbbr: faker.address.stateAbbr(),
-    zipCode: faker.address.zipCode(),
-    country: faker.address.country(),
+    street: faker.faker.location.streetAddress(),
+    city: faker.faker.location.city(),
+    stateAbbr: faker.faker.location.state({ abbreviated: true }),
+    zipCode: faker.faker.location.zipCode(),
+    country: faker.faker.location.country()
   }
 
-  const formattedDate = faker.date.past().toISOString().replace(/-/g, ``).replace(/:/g, ``).replace(/\..*$/, ``) + `Z`
+  const formattedDate = faker.faker.date.past().toISOString().replace(/-/g, ``).replace(/:/g, ``).replace(/\..*$/, ``) + `Z`
+  const url = faker.faker.image.url({width:300, height:300})
 
-  base64Img.requestBase64(faker.image.avatar(), (err, res, body) => {
-    if (err) console.error(err)
-
+  imageToBase64(url).then((body) => {
     const avatar = body.replace(`data:image/jpeg;base64,`, ``)
-
+    
     /* eslint-disable */
     if (i % 2 === 0) {
     fs.appendFileSync(contactsFile, `BEGIN:VCARD
 VERSION:3.0
-N:${lastName};${firstName};;${faker.name.prefix()};
+N:${lastName};${firstName};;${faker.faker.person.prefix()};
 FN:${firstName} ${lastName}
-ORG:${faker.company.companyName()}
-TITLE:${faker.name.jobTitle()}
+ORG:${faker.faker.company.name()}
+TITLE:${faker.faker.person.jobTitle()}
 PHOTO;PHOTO;ENCODING=b;TYPE=JPEG:${avatar}
-TEL;TYPE=HOME,VOICE:${faker.phone.phoneNumberFormat()}
-TEL;TYPE=WORK,VOICE:${faker.phone.phoneNumberFormat()}
+TEL;TYPE=HOME,VOICE:${fakerv0.phone.phoneNumberFormat()}
+TEL;TYPE=WORK,VOICE:${fakerv0.phone.phoneNumberFormat()}
 ADR;TYPE=HOME,PREF:;;${address1.street};${address1.city};${address1.stateAbbr};${address1.zipCode};${address1.country}
 LABEL;TYPE=HOME,PREF:${address1.street}\n${address1.city}\, ${address1.stateAbbr} ${address1.zipCode}\n${address1.country}
 ADR;TYPE=WORK:;;42 Plantation St.;${address2.city};${address2.stateAbbr};${address2.zipCode};${address2.country}
 LABEL;TYPE=WORK:42 Plantation St.\n${address2.city}\, ${address2.stateAbbr} ${address2.zipCode}\n${address2.country}
-EMAIL;TYPE=HOME:${faker.internet.exampleEmail().toLowerCase()}
-EMAIL;TYPE=WORK:${faker.internet.exampleEmail().toLowerCase()}
+EMAIL;TYPE=HOME:${faker.faker.internet.exampleEmail().toLowerCase()}
+EMAIL;TYPE=WORK:${faker.faker.internet.exampleEmail().toLowerCase()}
 REV:${formattedDate}
 END:VCARD
 
@@ -62,18 +62,20 @@ END:VCARD
 VERSION:3.0
 N:${lastName};${firstName};;;
 FN:${firstName} ${lastName}
-ORG:${faker.company.companyName()}
-TITLE:${faker.name.jobTitle()}
+ORG:${faker.faker.company.name()}
+TITLE:${faker.faker.person.jobTitle()}
 PHOTO;PHOTO;ENCODING=b;TYPE=JPEG:${avatar}
-TEL;TYPE=HOME,VOICE:${faker.phone.phoneNumberFormat()}
+TEL;TYPE=HOME,VOICE:${fakerv0.phone.phoneNumberFormat()}
 ADR;TYPE=HOME,PREF:;;${address1.street};${address1.city};${address1.stateAbbr};${address1.zipCode};${address1.country}
 LABEL;TYPE=HOME,PREF:${address1.street}\n${address1.city}\, ${address1.stateAbbr} ${address1.zipCode}\n${address1.country}
-EMAIL;TYPE=HOME:${faker.internet.exampleEmail().toLowerCase()}
+EMAIL;TYPE=HOME:${faker.faker.internet.exampleEmail().toLowerCase()}
 REV:${formattedDate}
 END:VCARD
 
 `)
     }
     /* eslint-enable */
-  })
+  }).catch((error)=>{
+  console.error("error", error)
+})
 }
